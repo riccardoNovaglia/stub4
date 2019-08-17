a7461164a8d25385a28134a73e2cdbd1c50c8ea2
Riccardo Novaglia <riccardo.novaglia@gmail.com>
2019-08-17 10:51:20 +0100

    Change stub request format, add prettier. Make things pretty

--- server/app.js
+++ server/app.js
@@ -1,26 +1,30 @@
 const express = require('express');
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/test', function(req, res) {
-  res.status(200).json({ hello: 'world' });
+    res.status(200).json({ hello: 'world' });
 });
 
-app.post('/new-stub', function(req, res, err) {
-    const {method, url, responseBody} = req.body;
+app.post('/new-stub', function(req, res) {
+    const method = _.get(req.body, 'requestMatchers.method', 'GET');
+    const url = _.get(req.body, 'requestMatchers.url');
 
-    if (!method || method === "GET") {
+    const body = _.get(req.body, 'response.body', {});
+
+    if (!method || method === 'GET') {
         app.get(url, (_, myRes) => {
-            return myRes.json(responseBody);
+            return myRes.json(body);
         });
     } else if (method === 'POST') {
         app.post(url, (_, myRes) => {
-            return myRes.json(responseBody);
+            return myRes.json(body);
         });
     }
-    
+
     res.sendStatus(200);
 });
 
--- server/app.test.js
+++ server/app.test.js
@@ -2,16 +2,10 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('Something', () => {
-    it('starts', async () => {
-        await request(app)
-            .get('/test')
-            .expect(200);
-    });
-
     it('creates new stubs', async () => {
         const stubCreationResponse = await request(app)
             .post('/new-stub')
-            .send({ url: '/john' });
+            .send({ requestMatchers: { url: '/john' } });
         expect(stubCreationResponse.status).toEqual(200);
 
         const stubbedResponse = await request(app).get('/john');
@@ -21,21 +15,26 @@ describe('Something', () => {
     it('responds from the new stub with the given body', async () => {
         await request(app)
             .post('/new-stub')
-            .send({ url: '/whateva', responseBody: { hey: "you!" } });
+            .send({
+                requestMatchers: { url: '/whateva' },
+                response: { body: { hey: 'you!' } }
+            });
 
         const response = await request(app).get('/whateva');
         expect(response.status).toEqual(200);
-        expect(response.body).toEqual({ hey: "you!" });
+        expect(response.body).toEqual({ hey: 'you!' });
     });
-
 
     it('sets up the stub with the required method', async () => {
         await request(app)
             .post('/new-stub')
-            .send({ method: "POST", url: '/another-one', responseBody: { just: "posted" } });
+            .send({
+                requestMatchers: { method: 'POST', url: '/another-one' },
+                response: { body: { just: 'posted' } }
+            });
 
         const response = await request(app).post('/another-one');
         expect(response.status).toEqual(200);
-        expect(response.body).toEqual({ just: "posted" });
+        expect(response.body).toEqual({ just: 'posted' });
     });
 });
