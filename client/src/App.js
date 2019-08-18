c16926481a3e5bc0443757f253c20c990dfa6a92
Riccardo Novaglia <riccardo.novaglia@gmail.com>
2019-08-18 12:23:09 +0100

    Just proving the point

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 
 function App() {
   const [response, setResponse] = useState('');
+  const [count, setCount] = useState(0);
 
   const setup = async () => {
+    setCount(count + 1);
     const res = await axios.post('/new-stub', {
       requestMatcher: { url: '/tis-new' },
-      response: { type: 'text', body: 'a new one' }
+      response: { type: 'text', body: `a new one ${count}` }
     });
     console.log('Got a res', res);
   };
