93ccd5812ea47872a57f51a3b112e112909f0a27
Riccardo Novaglia <riccardo.novaglia@gmail.com>
2019-09-06 16:33:16 +0200

    Stylez!

--- client/src/stubs/Stubs.js
+++ client/src/stubs/Stubs.js
@@ -1,19 +1,41 @@
+import _ from 'lodash';
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Stub.scss';
 
 export function Stubs({ stubs }) {
+  const [selected, setSelected] = useState();
+
   return (
-    <div>
-      {Object.keys(stubs).map(stub => (
-        <div key={`${stub}-fragment`} className="stub">
-          <p>METHOD: {stubs[stub].request.method}</p>
-          <p>URL: {stubs[stub].request.url}</p>
-          <p>CONTENT-TYPE: {stubs[stub].response.contentType}</p>
-          <p>BODY: {stubs[stub].response.body}</p>
+    <div className="stubs">
+      <div className="stubsList">
+        {_.isEmpty(stubs) && <p className="noStubsMsg">No stubs have been created yet</p>}
+        {Object.keys(stubs).map(stubUrl => (
+          <div key={`${stubUrl}-item`} className="stub" onClick={() => setSelected(stubs[stubUrl])}>
+            <p
+              className={
+                selected && selected.request.url === stubUrl ? 'selectedStubDef' : 'stubDef'
+              }
+            >
+              <span className="method">{stubs[stubUrl].request.method}</span>{' '}
+              <span className="url">{stubs[stubUrl].request.url}</span>
+              <span>→</span>
+              <span className="contentType">{stubs[stubUrl].response.contentType}</span>
+            </p>
+          </div>
+        ))}
+      </div>
+      {selected && (
+        <div className="selectedStub">
+          <div>
+            {selected.request.method} {selected.request.url}
+          </div>
+          <div>↓</div>
+          <div>{selected.response.contentType}</div>
+          <div>{selected.response.body}</div>
         </div>
-      ))}
+      )}
     </div>
   );
 }
