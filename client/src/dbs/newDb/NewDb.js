f685e838080fccca5b947ebbebf788e8a3fe3113
Riccardo Novaglia <riccardo.novaglia@gmail.com>
2019-09-12 07:48:28 +0100

    Can create dbs

--- client/src/dbs/newDb/NewDb.js
+++ client/src/dbs/newDb/NewDb.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import * as dbClient from './DbClient';
 
 import './NewDb.scss';
 
 export function NewDb({ afterSuccessfulCreation }) {
+  const [url, setUrl] = useState('');
+  const [idAlias, setIdAlias] = useState(undefined);
+
   const setup = async () => {
+    dbClient.createDb(`/${url}`, idAlias);
     afterSuccessfulCreation();
   };
+
+  const handle = setFn => event => setFn(event.target.value);
 
   return (
     <div className="newDb">
       <button onClick={setup}>Save</button>
+
+      <div>
+        <label htmlFor="url">URL</label>
+        <span className="leadingSlash">/</span>
+        <input id="url" type="text" onChange={handle(setUrl)} value={url} />
+      </div>
+
+      <div>
+        <label htmlFor="idAlias">id alias (optional)</label>
+        <input id="idAlias" type="text" onChange={handle(setIdAlias)} value={idAlias} />
+      </div>
     </div>
   );
 }
