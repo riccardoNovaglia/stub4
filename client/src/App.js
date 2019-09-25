a8e0cde662fdac243674c8b876f1847b1e83cd54
Riccardo Novaglia <riccardo.novaglia@gmail.com>
2019-09-25 06:49:37 +0100

    Silly button to create contracts

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 import { Stubs } from './stubs/Stubs';
@@ -7,15 +8,26 @@ import './App.scss';
 
 function App() {
   const [tab, setCurrentTab] = useState('stubs');
+  const [pactsCreated, setPactsCreated] = useState(false);
+
+  async function contracts() {
+    await generateContracts();
+    setPactsCreated(true);
+  }
 
   return (
     <div className="App">
       <button onClick={() => setCurrentTab('stubs')}>Stubs</button>
       <button onClick={() => setCurrentTab('cruds')}>Cruds</button>
+      <button onClick={() => contracts()}>Generate Contracts</button> {pactsCreated && <p>Ok</p>}
       {tab === 'stubs' && <Stubs />}
       {tab === 'cruds' && <Cruds />}
     </div>
   );
 }
+
+const generateContracts = async () => {
+  await axios.post('/generate-pact', { consumer: 'SomeApp' });
+};
 
 export default App;
