96206d41ebcce8e4b53793f9167ede42bcd3a40e
Riccardo Novaglia <riccardo.novaglia@gmail.com>
2020-02-16 11:57:42 +0000

    Bumps for new version. Update acceptance config because taiko doesn't like localhost:80

--- acceptance/config.js
+++ acceptance/config.js
@@ -2,6 +2,6 @@ const uiPort = process.env.UI_PORT;
 const stubsPort = process.env.STUBS_PORT;
 
 module.exports = {
-  appUrl: `http://localhost:${uiPort}`,
+  appUrl: uiPort === '80' ? 'http://localhost' : `http://localhost:${uiPort}`,
   stubsUrl: `http://localhost:${stubsPort}`
 };
