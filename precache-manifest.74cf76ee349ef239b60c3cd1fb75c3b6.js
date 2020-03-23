2ea6dfb1841e36a314f9378c1958a0cadfa3f684
Riccardo Novaglia <riccardo.novaglia@gmail.com>
2020-03-23 10:55:37 +0000

    Updates

---
+++ precache-manifest.74cf76ee349ef239b60c3cd1fb75c3b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377e0a916ecbf9e3004ba86caef3db31",
+    "url": "/stub4/index.html"
+  },
+  {
+    "revision": "000b5e0b89ca4df320a0",
+    "url": "/stub4/static/css/main.89d8e3f2.chunk.css"
+  },
+  {
+    "revision": "d8bf41aa699e9bad2061",
+    "url": "/stub4/static/js/2.420a7909.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/stub4/static/js/2.420a7909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "000b5e0b89ca4df320a0",
+    "url": "/stub4/static/js/main.ed029480.chunk.js"
+  },
+  {
+    "revision": "ca2d27c29f551002b031",
+    "url": "/stub4/static/js/runtime-main.0a7caa53.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stub4/precache-manifest.96097cb97e0617d14d8887a5df493d9d.js"
+  "/stub4/precache-manifest.74cf76ee349ef239b60c3cd1fb75c3b6.js"
 );
 
 self.addEventListener('message', (event) => {
