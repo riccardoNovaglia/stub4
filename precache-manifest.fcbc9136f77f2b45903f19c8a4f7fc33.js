68ba1d038e44005c78c48c66b0422132588c3ad8
Riccardo Novaglia <riccardo.novaglia@gmail.com>
2020-10-29 08:30:47 +0000

    Updates

---
+++ precache-manifest.fcbc9136f77f2b45903f19c8a4f7fc33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9db85c6314ad1df81a241ef75ecb5a3",
+    "url": "/stub4/index.html"
+  },
+  {
+    "revision": "efb18df2eed9a7062f9e",
+    "url": "/stub4/static/css/main.e5090539.chunk.css"
+  },
+  {
+    "revision": "1b1ee5826971b56b2907",
+    "url": "/stub4/static/js/2.19801c26.chunk.js"
+  },
+  {
+    "revision": "a680856a4903381d69a83276068aa227",
+    "url": "/stub4/static/js/2.19801c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb18df2eed9a7062f9e",
+    "url": "/stub4/static/js/main.6519f5bc.chunk.js"
+  },
+  {
+    "revision": "ec15a40ebe7b0f1229e3",
+    "url": "/stub4/static/js/runtime-main.4fd4ca2c.js"
+  }
+]);
